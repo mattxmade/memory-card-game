3bf393cd7b03766553e5a0878e724f233af41078
Matt <97047080+mattxmade@users.noreply.github.com>
2022-09-09 14:39:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import "./style.css";
 import App from "./components/App";
+import AppA from "./components/AppA";
 
 const root = createRoot(document.querySelector("#root"));
 root.render(
