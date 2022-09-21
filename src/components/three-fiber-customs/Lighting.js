b14e7c07074b3c56de283096a50c81317afb3f5e
Matt <97047080+mattxmade@users.noreply.github.com>
2022-09-21 09:39:30 +0100

    Create component; setup Canvas scene lighting

---
+++ src/components/three-fiber-customs/Lighting.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Lighting = (props) => (
+  <>
+    <directionalLight
+      args={["0xffffff"]}
+      color="whitesmoke"
+      position={[-2, 0, 150]}
+      intensity={0.5}
+    />
+    <ambientLight intensity={0.5} />
+    <spotLight
+      position={[1, 10, 25]}
+      penumbra={1}
+      angle={0.2}
+      color="white"
+      castShadow
+      intensity={1}
+      shadow-mapSize={[512, 512]}
+    />
+    <directionalLight position={[0, -15, -0]} intensity={4} color="steelblue" />
+  </>
+);
+
+export default Lighting;
