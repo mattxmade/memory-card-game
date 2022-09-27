356b2276a8bce1a21d6dac7da8ec2a239fd2e630
Matt <97047080+mattxmade@users.noreply.github.com>
2022-09-27 15:42:43 +0100

    Create Scoreboard component; handle props, render view

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,62 +1,22 @@
 import React from "react";
 
-const levelNumberStyle = {
-  width: "35px",
-  height: "35px",
-  cursor: "pointer",
-  borderRadius: "100%",
+const Item = ({ heading, level, score }) => (
+  <div className="scoreboard__item">
+    <h3>{heading}</h3>
+    <div className={"scoreboard__info"}>
+      <p className={`scoreboard__info_${heading.toLowerCase()}`}>
+        {heading === "Level" ? level : `${score}/${level * 5}`}
+      </p>
+    </div>
+  </div>
+);
 
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-};
-
-const LevelNumber = ({ index, active }) => {
-  if (active)
-    return (
-      <div className={"scoreboard__page-index"} style={levelNumberStyle}>
-        <p>{index}</p>
-      </div>
-    );
-};
-
-const scoreboardStyle = {
-  width: "100px",
-  height: "100%",
-  marginLeft: "2.5px",
-  position: "absolute",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  flexDirection: "column-reverse",
-
-  cursor: "auto",
-
-  item: {
-    width: "100%",
-    height: "100%",
-    gap: "2px",
-    display: "flex",
-    flexWrap: "wrap",
-    alignItems: "center",
-    placeContent: "center",
-    justifyContent: "center",
-  },
-};
-
-const Scoreboard = ({ level }) => {
+const Scoreboard = ({ level, score }) => {
   return (
-    <aside style={scoreboardStyle}>
-      <div style={scoreboardStyle.item}></div>
-      <div style={scoreboardStyle.item}></div>
-      <div className="scoreboard__item" style={scoreboardStyle.item}>
-        {[...new Array(10)].map((item, i) => {
-          const active = i < level ? true : false;
-
-          return <LevelNumber key={i} index={i + 1} active={active} />;
-        })}
-      </div>
-    </aside>
+    <div className="scoreboard">
+      <Item heading={"Level"} level={level} />
+      <Item heading={"Cards"} level={level} score={score} />
+    </div>
   );
 };
 
