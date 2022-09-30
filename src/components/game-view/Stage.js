833f5fae4c095609ede525aa809c5a0734d8cfff
Matt <97047080+mattxmade@users.noreply.github.com>
2022-09-30 18:23:15 +0100

    Add SVG component + Structure components to Stage scene

--- src/components/game-view/Stage.js
+++ src/components/game-view/Stage.js
@@ -2,6 +2,23 @@ import React from "react";
 import { Plane } from "@react-three/drei";
 import degreesToRadian from "./utility/degressToRadian";
 
+import SVG from "./SVG";
+import logo from "../../svg/logo.svg";
+
+const Structure = (props) => (
+  <mesh position={props.position}>
+    <boxGeometry args={[1, 1, 15]} />
+    <meshPhysicalMaterial attach="material" color={props.color} />
+  </mesh>
+);
+
+const Wall = ({ scale, position, rotation, color }) => (
+  <mesh position={position} rotation={rotation}>
+    <boxGeometry args={scale} receiveShadow />
+    <meshPhysicalMaterial attach="material" color={color} />
+  </mesh>
+);
+
 const Stage = (() => {
   const gridBoxA = ({ rotation, orientateGrid, orientateAxis }) => (
     <group rotation={rotation} position={[0, 0, -9]}>
@@ -11,28 +28,20 @@ const Stage = (() => {
         scale={[1.5, 3, 1]}
       />
 
-      <Plane // right wall panels
-        receiveShadow
-        scale={[15, 8, 1]}
+      <Wall // right
+        color="white"
+        scale={[15, 8, 0.5]}
         position={[7.5, 2.5, 0]}
         rotation={degreesToRadian([90, -90, 90])}
-      >
-        <meshPhongMaterial attach="material" color="#ffffff" />
-      </Plane>
-
-      <gridHelper // right grid
-        position={[7.5, 2.5, 0]}
-        rotation={degreesToRadian([90, 0, 90])}
-        scale={[1.5, 3, 1]}
       />
 
       <Plane // ground
         scale={[15, 15, 1]}
         receiveShadow
-        position={[0, -2.5, 1.5]}
+        position={[0, -2.5, 0]}
         rotation={[orientateAxis, 0, 0]}
       >
-        <meshPhongMaterial attach="material" color="#ffffff" />
+        <meshPhongMaterial attach="material" color="white" />
       </Plane>
 
       <gridHelper
@@ -40,6 +49,17 @@ const Stage = (() => {
         rotation={[0, 0, 0]}
         scale={(1.5, 1.5, 1.5)}
       />
+
+      <SVG
+        url={logo}
+        scale={0.01}
+        position={[1.4, 5.5, -16.5]}
+        rotation={[0, -0.55, 0]}
+        wireframe={true}
+      />
+
+      <Structure position={[7.5, 7, 0]} color={"white"} />
+      <Structure position={[7.5, -2, 0]} color={"lightgrey"} />
     </group>
   );
 
@@ -65,7 +85,7 @@ const Stage = (() => {
         position={[0, -2.5, 1.5]}
         rotation={[orientateAxis, 0, 0]}
       >
-        <meshPhongMaterial attach="material" color="#ffffff" />
+        <meshPhongMaterial attach="material" color="white" />
       </Plane>
 
       <gridHelper
