0eb0e02813edf8c082320f585c8bd756d05b144c
Matt <97047080+mattxmade@users.noreply.github.com>
2022-09-30 17:44:14 +0100

    Add additional className

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./styles/scoreboard.css";
 
 const Item = ({ heading, level, score }) => (
-  <div className="scoreboard__item">
+  <div className="scoreboard__item heads-up-display__item">
     <h3>{heading}</h3>
     <div className={"scoreboard__info"}>
       <p className={`scoreboard__info_${heading.toLowerCase()}`}>
@@ -13,7 +14,7 @@ const Item = ({ heading, level, score }) => (
 
 const Scoreboard = ({ level, score }) => {
   return (
-    <div className="scoreboard">
+    <div className="scoreboard heads-up-display">
       <Item heading={"Level"} level={level} />
       <Item heading={"Cards"} level={level} score={score} />
     </div>
